da832a8583ebdfeea23e1a1d65f99502d7b0668a
Prajwal Shah <prajwalshah1721@gmail.com>
2020-08-01 01:20:35 +0530

    Game Ready

--- src/components/guess/input.jsx
+++ src/components/guess/input.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './input.scss'
 import { History } from '../history/history';
 import FormInput from '../form-input/form-input';
+import {ResponseBack} from '../response/response';
 class InputBox extends React.Component {
     constructor({ min = 1, max = 100 }) {
         super();
@@ -13,48 +14,71 @@ class InputBox extends React.Component {
             display: false,
             arr: [],
             area: '',
+            direction:'',
+            color:'white'
         }
         this.onSubmit = this.onSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
         this.change = this.change.bind(this);
+        this.check = this.check.bind(this);
+        this.data = this.data.bind(this);
     };
-    check =function() {
-        const number = this.state.number, guess = this.state.guess;
+    check =()=> {
+        const number = parseInt(this.state.number), guess = this.state.guess;
         if (number > guess) {
-            
+            this.setState({direction:'high'})
         }
         if (number < guess) {
-            
+            this.setState({direction:'low'})
         }
         const band= [1, 4, 15];
-        const fact = Math.round(this.state.max/100), diff = Math.abs(number - guess);
-        
+        const fact = Math.ceil(this.state.max/100);
+        const diff = Math.abs(number - guess);
         switch (true) {
-            case diff:
-                area: 'correct'
+            case diff===0:
+                this.setState({area:'correct',
+                direction:''
+            })
                 break;
-            case (diff <= band[1]):
-                area: 'hot'
+            case diff <= band[1]*fact:
+                this.setState({area:'hot'})
                 break;
-            case (diff <= band[2]):
-                area: 'warm'
+            case diff <= band[2]*fact:
+                this.setState({area:'warm'})
                 break;
-            default:
-                area: 'cold'
+            case diff > band[2]*fact:
+                this.setState({area:'cold'})
                 break;
+            default:break;
         }
         
     };
-
     change = async () => {
         await this.state.arr.push(this.state.number);
+        await this.check();
         await this.setState({ number: '' });
+        await this.data(this.state.area);
     };
-    
+    data=(area)=>{
+        var color;
+        switch(area){
+            case 'correct':
+                color='green';break;
+            case 'hot':
+                color='red';break;
+            case 'warm':
+                color='yellow';break;
+            case 'cold':
+                color='blue';break;
+            default :
+                color='white';break;
+        }
+        this.setState({color:color})
+    }
     onSubmit = (e) => {
         e.preventDefault();
-        if (this.state.number === this.state.guess) { }
         this.change();
+
     };
     handleChange = (e) => {
         var num = parseInt(e.target.value)
@@ -102,8 +126,12 @@ class InputBox extends React.Component {
                     </form>
                 </div>
                 <History arr={this.state.arr} />
-                {/* {this.state.arr.length?this.state.arr.map((n)=>`${n}  `):'Start'} */}
-
+                <ResponseBack text={this.state.area} direction={this.state.direction} color= {this.state.color} />
+                <button></button>
+                {(this.state.area==='correct'?<div>
+                    <button>End</button>
+                    <button>Next Level</button>
+                    </div>:null)}
             </div>
         )
     }
---
+++ src/components/response/response.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+export const  ResponseBack = ({color,direction,text})=>
+                (<div style={{backgroundColor:color}}>{`${text} ${direction?`On the ${direction} side !`:' '}`}</div>)   
+        
+
